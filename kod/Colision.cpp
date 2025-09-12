94fbbc6c939231d77fceeac79644c24052bd4b33
Rudy-debug117 <michal.stachowiak2006@gmail.com>
2025-09-12 20:03:20 +0200

    Delete kod directory

--- kod/Colision.cpp
+++
@@ -1,9 +0,0 @@
-#include "Collision.h"
-#include "Entity.h"
-
-bool isCollide(Entity* a, Entity* b)
-{
-    return (b->x - a->x) * (b->x - a->x) +
-        (b->y - a->y) * (b->y - a->y) <
-        (a->R + b->R) * (a->R + b->R);
-}
