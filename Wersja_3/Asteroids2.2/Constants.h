fc37726739088906e8b1335666b7d4e33dfc6686
Rudy-debug117 <michal.stachowiak2006@gmail.com>
2025-09-15 19:52:42 +0200

    Update and rename Wersja_3/Asteroids2.2/Constants.h to Wersja_3/Constants.h

--- Wersja_3/Asteroids2.2/Constants.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#ifndef CONSTANTS_H
-#define CONSTANTS_H
-
-const int W = 1200;            // Szerokoœæ okna
-const int H = 800;             // Wysokoœæ okna
-const float DEGTORAD = 0.017453f; // Sta³a do konwersji stopni na radiany
-
-#endif
---
+++ Wersja_3/Constants.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+const int W = 1200;           
+const int H = 800;            
+const float DEGTORAD = 0.017453f;
+
+#endif
+
