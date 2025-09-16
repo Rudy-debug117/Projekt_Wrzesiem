3f0081b71d12815030315b7f93abb45c9e79831d
pierozek-exe <piotr.kuczma205@gmail.com>
2025-09-16 21:25:11 +0200

    Add files via upload

---
+++ Wersja_4/Constants.h
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+const int W = 1200;            // Szerokoœæ okna
+const int H = 800;             // Wysokoœæ okna
+const float DEGTORAD = 0.017453f; // Sta³a do konwersji stopni na radiany
+
+#endif
