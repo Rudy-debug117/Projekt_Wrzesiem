c0fd7c0c1a66a0fbb3879234b5197b5eb9da400e
pierozek-exe <piotr.kuczma205@gmail.com>
2025-09-17 21:16:55 +0200

    Add files via upload

---
+++ Wersja_5/Collision.cpp
@@ -0,0 +1,9 @@
+#include "Collision.h"
+#include "Entity.h"
+
+bool isCollide(Entity* a, Entity* b)
+{
+    return (b->x - a->x) * (b->x - a->x) +
+        (b->y - a->y) * (b->y - a->y) <
+        (a->R + b->R) * (a->R + b->R);
+}
